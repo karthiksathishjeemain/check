7ab6a21ed187dbe4965ee996c32e94061a680e8c
KARTHIK SATHISH <karthiksathishjeemain@gmail.com>
2024-12-07 11:42:06 +0530

    few modofications

--- st4.py
+++ st4.py
@@ -1,9 +1,10 @@
 # from groq import Groq
 import streamlit as st
-from importnb import Notebook
 
-with Notebook():
-    import rag
+# from importnb import Notebook
+
+# with Notebook():
+#     import rag
 
 # st.set_page_config(layout="wide")
 # Custom CSS to center the title
@@ -30,31 +31,26 @@ st.write("")
 st.logo("./icon.png", size="large")
 
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [
-       
-    ]
-if (len(st.session_state.messages)==0):
+    st.session_state["messages"] = []
+if len(st.session_state.messages) == 0:
     st.markdown("## How can I help you?")
 
 
+if len(st.session_state.messages) != 0:
+    for msg in st.session_state.messages:
+        st.chat_message(msg["role"]).write(msg["content"])
+if prompt := st.chat_input("Enter your Message"):
 
-for msg in st.session_state.messages:
-       st.chat_message(msg["role"]).write(msg["content"])
-if prompt:=st.chat_input("Enter your Message"):
-   
-
-    st.session_state.messages.append(
-        {"role": "user", "content": prompt}
-    )
+    st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
 
-    config = {"configurable": {"thread_id": "7896323"}}
-    answer = rag.langgraph_agent_executor.invoke(
-        {"messages": [("user", prompt)]},
-        config,
-    )["messages"][-1]
+    # config = {"configurable": {"thread_id": "7896323"}}
+    # answer = rag.langgraph_agent_executor.invoke(
+    #     {"messages": [("user", prompt)]},
+    #     config,
+    # )["messages"][-1]
 
-    msg = answer.content
+    # msg = answer.content
 
-    st.session_state.messages.append({"role": "assistant", "content": msg})
-    st.chat_message("assistant").write(msg)
+    st.session_state.messages.append({"role": "assistant", "content": "echo " + prompt})
+    st.chat_message("assistant").write("echo " + prompt)
